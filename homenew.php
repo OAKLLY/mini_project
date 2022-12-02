dd7d4a93faf5b36d955d683b246f44bc64bc1531
OAKLLY <krisada3200@gmail.com>
2022-12-03 01:09:46 +0700

    Fn

---
+++ homenew.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>RavenClaw Place</title>
+    <style>
+        h1{
+            position:relative;
+            top: 120px;
+            font-family: 'Prompt', sans-serif;
+        }
+        span{
+            color:#ffd60a;
+        }
+        button{
+            top: 200px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class = "container" style="margin-top: 20px;">
+    <ul class="list-inline text-end">
+        <li class="btn btn-primary me-1">Home</li>
+        <li class="btn btn-primary me-1">Reserve</li>
+        <li class="btn btn-primary">About</li>
+    </ul>
+        <div class="welcome-text text-center">
+            <h1 class="display-3">Welcome to <br><span> RavenClaw <br> Place</span></h1>          
+           <div class="d-grid gap-2 col-6 mx-auto">
+            <button class="btn btn-primary" type="button">Button</button>
+            <button class="btn btn-primary" type="button">Button</button>
+        </div>
+    </div>
+
+    </div>
+    
+</body>
+</html>
